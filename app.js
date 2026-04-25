2130b7f41f7565e2892e98b59b4ceef563c0d018
dunasad <admi.dunas@gmail.com>
2026-04-24 20:50:45 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,36 +1,16 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
 
-// 👇 pega tus datos aquí
 const supabase = createClient(
   'https://coywogyelfaspxlsctjv.supabase.co',
-  'sb_publishable_q0ALHHNo6mvf2AkNq5EcwA_F3QxgzRr'
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNveXdvZ3llbGZhc3B4bHNjdGp2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzcwNTkzMDksImV4cCI6MjA5MjYzNTMwOX0.BXT3hpn9CZevtc39KEVzkwyhjfCQ_087eyNp5UuFTS8' // tu key completa
 )
 
 async function guardarNota() {
-  const cliente = document.getElementById("cliente").value
-  const descripcion = document.getElementById("descripcion").value
-  const cantidad = Number(document.getElementById("cantidad").value)
-  const precio = Number(document.getElementById("precio").value)
-
-  const total = cantidad * precio
-
   const { data, error } = await supabase
     .from('notas')
-    .insert([
-      {
-        descripcion,
-        cantidad,
-        precio,
-        total,
-        fecha: new Date()
-      }
-    ])
+    .insert([{ descripcion: "prueba" }])
 
-  if (error) {
-    alert("Error: " + error.message)
-  } else {
-    alert("Nota guardada ✅")
-  }
+  console.log(data, error)
 }
 
 window.guardarNota = guardarNota
