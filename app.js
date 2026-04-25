6536da28fc593cc08b2ec0ed83b4bac8ebfd5e01
dunasad <admi.dunas@gmail.com>
2026-04-25 13:20:49 -0600

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js
 
 const supabase = createClient(
   'https://coywogyelfaspxlsctjv.supabase.co',
-  'TU_KEY_AQUI'
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNveXdvZ3llbGZhc3B4bHNjdGp2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzcwNTkzMDksImV4cCI6MjA5MjYzNTMwOX0.BXT3hpn9CZevtc39KEVzkwyhjfCQ_087eyNp5UuFTS8
+'
 )
 
 // SIDEBAR
